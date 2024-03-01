a68ba477ddec4d7de44b055c7a1e1317951932dd
codehedgehog <codehedgehog@outlook.com>
2024-02-29 21:14:21 -0800

    Add ErrorHandler component

--- Chapter02/Demo/Demo/Pages/CounterCodeBehind.razor.cs
+++ Chapter02/Demo/Demo/Pages/CounterCodeBehind.razor.cs
@@ -2,22 +2,23 @@ using Microsoft.AspNetCore.Components;
 
 namespace Demo.Pages
 {
-    public partial class CounterCodeBehind
+  public partial class CounterCodeBehind
+  {
+    [Parameter]
+    public int? Increment { get; set; }
+
+    private int increment = 1;
+    private int currentCount = 0;
+
+    protected override void OnParametersSet()
     {
-        [Parameter]
-        public int? Increment { get; set; }
-
-        private int increment = 1;
-        private int currentCount = 0;
-        protected override void OnParametersSet()
-        {
-            if (Increment.HasValue)
-                increment = Increment.Value;
-        }
-
-        private void IncrementCount()
-        {
-            currentCount += increment;
-        }
+      if (Increment.HasValue)
+        increment = Increment.Value;
     }
+
+    private void IncrementCount()
+    {
+      currentCount += increment;
+    }
+  }
 }
